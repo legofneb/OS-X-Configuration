c05bae828bff0545ee512af2d3939d4fc9bc5274
Benjamin Fogel <bnf0001@Ben-Mac.local>
2014-10-08 15:56:15 -0500

    Added Initial Install Script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Brew Casks
+brew install caskroom/cask/brew-cask
+
+# Programs
+brew cask install google-chrome
+brew cask install macdown
+
