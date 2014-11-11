0a0996342a7fce38cacc1c7da4f54cd28da7df39
Ben Fogel <legofneb@gmail.com>
2014-11-11 14:07:20 -0600

    Added Git customizations

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ brew cask install macdown
 brew cask install eclipse-ide
 brew cask install java
 
-#Install Chromium
+# Install Chromium Tols
 sudo mkdir /usr/bin/depot_tools
 sudo git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /usr/bin/depot_tools
 echo "export PATH=/usr/bin/depot_tools:\"\$PATH\"" >> ~/.zshrc
@@ -22,6 +22,8 @@ echo "export PATH=/usr/bin/depot_tools:\"\$PATH\"" >> ~/.zshrc
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true # disables DS_Store files on network drives
 echo "function gi() { curl -L -s https://www.gitignore.io/api/\$@ ;}" >> ~/.zshrc && source ~/.zshrc # gitignore.io configuration
 
+git config --global user.name "Ben Fogel"
+git config --global user.email "legofneb@gmail.com"
 
 # Let Homebrew manage openssl
 brew install openssl
